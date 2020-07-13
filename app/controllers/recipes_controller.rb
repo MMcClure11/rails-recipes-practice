60e358502f6acc1bfa28c5d4e616ae3ad6224b79
MMcClure11 <mcclme02@luther.edu>
2020-07-13 14:13:46 -0500

    adds new and create actions, views, and recipe params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,18 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, description:, instructions:, cook_time:, :prep_time)
+  end
 end
