20488a682bf0e6f1c7db552e1985a27fa4f68575
MMcClure11 <mcclme02@luther.edu>
2020-07-13 13:56:27 -0500

    adds index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :recipes
+
+  #get '/recipes', to: 'recipes#index'
+  # HTTP verb "route", to: "controller#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
