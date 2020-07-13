529199967eb8a6efcf21299843bb339eab288c52
MMcClure11 <mcclme02@luther.edu>
2020-07-13 14:20:14 -0500

    adds form for creating a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
 
   def index
     @recipes = Recipe.all
@@ -13,12 +14,21 @@ class RecipesController < ApplicationController
   end
 
   def create
-    
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
   private
 
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
   def recipe_params
-    params.require(:recipe).permit(:name, description:, instructions:, cook_time:, :prep_time)
+    params.require(:recipe).permit(:name, :description, :instructions, :cook_time, :prep_time)
   end
 end
